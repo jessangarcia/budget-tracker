4f04eb4b06d67ed581c89d89b43669ed53561a2e
jessan <jessenia.garcia998@gmail.com>
2021-11-10 02:48:51 -0700

    updated service worker to fix getAll error, moved files into assests folder

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,63 +1,81 @@
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = 'my-site-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v2';
 
 const FILES_TO_CACHE = [
-    './index.html',
-    './css/style.css',
-    './js/index.js',
-    './js/idb.js',
-    './manifest.json',
-    './icons/icon-512x512.png',
-    './icons/icon-384x384.png',
-    './icons/icon-192x192.png',
-    './icons/icon-152x152.png',
-    './icons/icon-144x144.png',
-    './icons/icon-128x128.png',
-    './icons/icon-96x96.png',
-    './icons/icon-72x72.png'
+    '/',
+    '/index.html',
+    '/assets/css/styles.css',
+    '/assets/js/index.js',
+    '/assets/js/idb.js',
+    '/manifest.json',
+    '/assets/icons/icon-512x512.png',
+    '/assets/icons/icon-384x384.png',
+    '/assets/icons/icon-192x192.png',
+    '/assets/icons/icon-152x152.png',
+    '/assets/icons/icon-144x144.png',
+    '/assets/icons/icon-128x128.png',
+    '/assets/icons/icon-96x96.png',
+    '/assets/icons/icon-72x72.png'
 ];
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
-});
-
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeepList = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            })
-
-            cacheKeepList.push(CACHE_NAME);
-
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeepList.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i]);
-                    return caches.delete(keyList[i]);
-                }
-            }));
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('files were pre-cached successfully');
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-
+self.addEventListener('activate', function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
         })
-    )
+    );
+});
+
+self.addEventListener('fetch', function (evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches
+                .open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(evt.request)
+                        .then(response => {
+                            if (response.status === 200) {
+                                cache.put(evt.request.url, response.clone());
+                            }
+
+                            return response;
+                        })
+                        .catch(err => {
+                            // Network request failed, try to get it from the cache.
+                            return cache.match(evt.request);
+                        });
+                })
+                .catch(err => console.log(err))
+        );
+        return;
+    }
+
+    evt.respondWith(
+        fetch(evt.request).catch(function () {
+            return caches.match(evt.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (evt.request.headers.get('accept').includes('text/html')) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match('/');
+                }
+            });
+        })
+    );
 });
