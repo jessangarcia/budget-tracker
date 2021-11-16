386c5ab61b3c254c07357a0cfc5b71ca2248f4f5
jessan <jessenia.garcia998@gmail.com>
2021-11-16 00:07:16 -0700

    moved server.js around

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
+
+
 
 app.use(logger("dev"));
 
@@ -16,13 +17,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
 // routes
 app.use(require("./routes/api.js"));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI);
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
