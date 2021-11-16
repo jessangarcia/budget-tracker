bf9c21b15c03cd0120d529a05f5993a7341376a8
jessan <jessenia.garcia998@gmail.com>
2021-11-16 00:51:41 -0700

    deploying with mongoatlas

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
-
-
 
 app.use(logger("dev"));
 
@@ -17,12 +16,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 // routes
 app.use(require("./routes/api.js"));
-
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI);
-
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
